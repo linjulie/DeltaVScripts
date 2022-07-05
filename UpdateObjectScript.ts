a348dae98f65e437061cd2db872701743f2776e4
linjulie <99178418+linjulie@users.noreply.github.com>
2022-07-05 14:22:07 +0200

    Update UpdateObjectScript.ts
    Update obeject with corresponding id..
    If nothing new is "committed" then alert will say that nothing has changed

--- UpdateObjectScript.ts
+++ UpdateObjectScript.ts
@@ -1,1 +1,31 @@
+async function returnPopUpArryy(id:any){
+    let qres = await SQL.SQLDatasource1.QueryAsync(GL.Library.S_PopUpQuery, id);
+    let qresArry:string[] = String(qres).split(",");
+    return qresArry;
+ 
+}
 
+async function updateObject(id:any, header:any, section:any){
+    let popUpBefore = await returnPopUpArryy(id);
+    let headerBefore = popUpBefore[0];
+    let sectionBefore = popUpBefore[1];
+
+    try{
+        if(header == headerBefore && section == sectionBefore){
+            alert("nothing new has changed")
+        }else{
+            let qres = SQL.SQLDatasource1.QueryAsync(GL.Library.S_UpdatePopUp, header, section, id);
+            alert("Object with id:" + id + " has been updated");
+            console.log("hellu")
+        }
+        
+    }catch(e){
+        alert(e.message);
+    }
+}
+
+let idValue = Dsp.UPDATE_VALUE_GEM1.ID;
+let headerValue = Dsp.UPDATE_VALUE_GEM1.Header;
+let sectionValue = Dsp.UPDATE_VALUE_GEM1.Section;
+
+updateObject(idValue, headerValue, sectionValue);
